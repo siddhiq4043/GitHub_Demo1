0d7a34803a2286d811da7afde1e5fa09ee1074f6
siddhiq4043 <siddhiq4043@gmail.com>
2021-06-14 09:50:27 +0530

    Added class B in GitHub_Demo1

---
+++ GitHub_Demo1/src/main/java/Java/B.java
@@ -0,0 +1,13 @@
+package Java;
+
+public class B {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		
+		System.out.println("Added class B in GitHub_Demo1");
+
+	}
+
+}
